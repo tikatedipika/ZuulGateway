2e198e52ae97df1996d962e940efed1662c03f9a
tikatedipika <tikate.dipika@gmail.com>
2019-06-11 13:55:01 -0400

    API gateway

--- src/main/java/com/cognizant/zuulgatewayproxy/filters/PreFilter.java
+++ src/main/java/com/cognizant/zuulgatewayproxy/filters/PreFilter.java
@@ -1,5 +1,7 @@
 package com.cognizant.zuulgatewayproxy.filters;
 
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 
 import com.netflix.zuul.ZuulFilter;
@@ -17,7 +19,18 @@ public class PreFilter extends ZuulFilter {
 	public Object run() throws ZuulException {
 		RequestContext ctx = RequestContext.getCurrentContext();
 	    HttpServletRequest request = ctx.getRequest();
-	 
+	    //;
+	    Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String name = headerNames.nextElement();
+			System.out.println("header name:"+name);
+			Enumeration<String> values = request.getHeaders(name);
+
+			while (values.hasMoreElements()) {
+				String value = values.nextElement();
+				System.out.println("Header Value"+value);
+			}
+		}
 	    System.out.println("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
 	    return null;
 	}
--- src/main/java/com/cognizant/zuulgatewayproxy/filters/RouteFilter.java
+++ src/main/java/com/cognizant/zuulgatewayproxy/filters/RouteFilter.java
@@ -1,6 +1,11 @@
 package com.cognizant.zuulgatewayproxy.filters;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
 public class RouteFilter extends ZuulFilter {
@@ -12,6 +17,20 @@ public class RouteFilter extends ZuulFilter {
 
 	@Override
 	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+	    HttpServletRequest request = ctx.getRequest();
+	    //;
+	    Enumeration<String> headerNames = request.getHeaderNames();
+		while (headerNames.hasMoreElements()) {
+			String name = headerNames.nextElement();
+			System.out.println("header name:"+name);
+			Enumeration<String> values = request.getHeaders(name);
+
+			while (values.hasMoreElements()) {
+				String value = values.nextElement();
+				System.out.println("Header Value"+value);
+			}
+		}
 		System.out.println("Inside Route Filter");
 		return null;
 	}
